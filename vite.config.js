4224650c86f09d4d050f46a54d37a7d8b1b36aed
Fure Denedo <adenedo@gmail.com>
2025-06-14 13:44:27 +0100

    Added build configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import path from 'path'
 export default defineConfig({
   root: './',
   publicDir: './public',
-  base: './',
-  optimizeDeps: {
-    include: [
-      'three',
-      'three/addons/controls/OrbitControls.js',
-      'three/examples/jsm/libs/stats.module.js'
-    ],
+  base: '/three-js-starter/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
   },
   resolve: {
     alias: {
